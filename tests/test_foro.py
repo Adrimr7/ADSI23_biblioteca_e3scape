964a45b4b143c6b12acf6d857cb8b97341719ad2
Etxaniz100 <79299525+Etxaniz100@users.noreply.github.com>
2023-12-22 11:57:07 +0100

    Cambio test foro

--- tests/test_foro.py
+++ tests/test_foro.py
@@ -1,5 +1,16 @@
 from . import BaseTestClass
 from bs4 import BeautifulSoup
+import sqlite3
+import os
+
+
+#Connection
+salt = "library"
+script_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(script_dir, '..', 'datos.db')
+
+con = sqlite3.connect(db_path)
+cur = con.cursor()
 
 class TestForo(BaseTestClass):
 
@@ -19,6 +30,7 @@ class TestForo(BaseTestClass):
 		res = self.client.get('/tema', query_string = params)
 		#print("test_temaInexistente status code : " + str(res.status_code))
 		self.assertEqual(302, res.status_code)
+		self.assertEqual('/', res.location)
 
 	def test_temaExiste(self):
 		params = {
@@ -33,11 +45,15 @@ class TestForo(BaseTestClass):
 
 	def test_crearTemaEstandoLogeado(self):
 		self.login('jhon@gmail.com', '123')
-		pass
+		res = self.client.get('/nuevoTema')
+		cur.execute(f"""INSERT INTO Tema VALUES ('10', 'TemaTest', 'jhon@gmail.com', 'En efecto, esto es un test')""")
+		con.commit()
 
 	def test_crearTemaSinLogear(self):
 		# entrar a crear foro -> redirige a /
-		pass
+		res = self.client.get('/nuevoTema')
+		self.assertEqual(302, res.status_code)
+		self.assertEqual('/', res.location)
 
 	def test_crearComentarioEstandoLogeado(self):
 		#logear -> entrar a comentar en un tema -> comentar -> comprobar en el tema
