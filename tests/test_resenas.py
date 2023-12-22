9275f8af56efccff46c59d44190f99f977573b4d
Jon Marcos Mercadé <104355567+Jonmaa@users.noreply.github.com>
2023-12-22 12:42:35 +0100

    test reseñas finalizados a priori

--- tests/test_resenas.py
+++ tests/test_resenas.py
@@ -35,6 +35,10 @@ class TestCatalogo(BaseTestClass):
 		con.commit()
 		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
 		self.assertEqual(0, len(res))
+		# Mirar con el BeautifulSoup para ver cuantas reseñas hay en el html
+		res = self.client.get('/resenas')
+		page = BeautifulSoup(res.data, features="html.parser")
+		self.assertEqual(0, len(page.find('div', class_='row').find_all('div', class_='card')))
 
 	def test_conResenas(self):
 		self.login('jhon@gmail.com', '123')
@@ -49,12 +53,13 @@ class TestCatalogo(BaseTestClass):
 		con.commit()
 		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
 		self.assertEqual(2, len(res))
-		cur.execute(f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '10', 'ok', '5.0')""")
-		con.commit()
-		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
-		self.assertEqual(3, len(res))
+		#Mirar con el BeautifulSoup para ver cuantas reseñas hay en el html
+		res = self.client.get('/resenas')
+		page = BeautifulSoup(res.data, features="html.parser")
+		self.assertEqual(2, len(page.find('div', class_='row').find_all('div', class_='card')))
 
-	def test_editarResena(self):
+
+	def test_anadirResena(self):
 		self.login('jhon@gmail.com', '123')
 		res = self.client.get('/resenas')
 		self.assertEqual(200, res.status_code)
@@ -66,6 +71,67 @@ class TestCatalogo(BaseTestClass):
 		con.commit()
 		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
 		self.assertEqual(2, len(res))
+		#Anadimos una reseña extra
+		cur.execute(f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '10', 'ok', '5.0')""")
+		con.commit()
+		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
+		self.assertEqual(3, len(res))
+		cur.execute(f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '11', 'not ok', '2.0')""")
+		con.commit()
+		cur.execute(f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '12', 'okkk', '9.0')""")
+		con.commit()
+		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
+		self.assertEqual(5, len(res))
+
+	def test_eliminarResena(self):
+		self.login('jhon@gmail.com', '123')
+		res = self.client.get('/resenas')
+		self.assertEqual(200, res.status_code)
+		# Comprobar que tiene reseñas escritas
+		cur.execute(f"""DELETE FROM Reseña""")
+		con.commit()
+		cur.execute(f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '6', 'muy recomendable para fans de la fantasia', '8.85')""")
+		con.commit()
+		cur.execute(f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '5', 'es un libro vomitivo', '1.1')""")
+		con.commit()
+		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
+		self.assertEqual(2, len(res))
+		cur.execute(f"""DELETE FROM Reseña WHERE idLibro = '5' """)
+		con.commit()
+		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
+		self.assertEqual(1, len(res))
+	def test_verResenaEnConcreto(self):
+		self.login('jhon@gmail.com', '123')
+		res = self.client.get('/resenas')
+		self.assertEqual(200, res.status_code)
+		cur.execute(f"""DELETE FROM Reseña""")
+		con.commit()
+		cur.execute(f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '5', 'es un libro vomitivo', '1.1')""")
+		con.commit()
+		params = {
+			'resena': "vomitivo"
+		}
+		params2 = {
+			'valoracion': "1.1"
+		}
+		res = self.client.get('/resenas', query_string=params)
+		page = BeautifulSoup(res.data, features="html.parser")
+		self.assertEqual(1, len(page.find('div', class_='row').find_all('div', class_='card')))
+		for card in page.find('div', class_='row').find_all('div', class_='card'):
+			self.assertIn(params['resena'].lower(), card.find(class_='card-text').get_text().lower())
+			self.assertIn(params2['valoracion'].lower(), card.find(class_='card-text2').get_text().lower())
+
+
+	def test_editarResena(self):
+		self.login('jhon@gmail.com', '123')
+		res = self.client.get('/resenas')
+		self.assertEqual(200, res.status_code)
+		cur.execute(f"""DELETE FROM Reseña""")
+		con.commit()
+		cur.execute(f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '5', 'es un libro vomitivo', '1.1')""")
+		con.commit()
+		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
+		self.assertEqual(1, len(res))
 		res = self.client.get('/editarResena')
 		self.assertEqual(200, res.status_code)
 		page = BeautifulSoup(res.data, features="html.parser")
@@ -74,31 +140,17 @@ class TestCatalogo(BaseTestClass):
 		self.assertIsNotNone(page.find('form').find('button', type='submit'))
 		cur.execute(f"""UPDATE Reseña SET resena = 'no tan malo' WHERE idLibro = '5' AND emailUser = 'jhon@gmail.com'""")
 		con.commit()
+		#Ahora pasamos a mirar que la resena haya cambiado en el html con el BeautifulSoup
 		res = self.client.get('/resenas')
 		self.assertEqual(200, res.status_code)
-
-
-
-	def test_cancelarEditResena(self):
-		self.login('jhon@gmail.com', '123')
-		res = self.client.get('/resenas')
-		self.assertEqual(200, res.status_code)
-		cur.execute(f"""DELETE FROM Reseña""")
-		con.commit()
-		cur.execute(
-			f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '6', 'muy recomendable para fans de la fantasia', '8.85')""")
-		con.commit()
-		cur.execute(f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '5', 'es un libro vomitivo', '1.1')""")
-		con.commit()
-		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
-		self.assertEqual(2, len(res))
-		res = self.client.get('/editarResena')
-		self.assertEqual(200, res.status_code)
+		params = {
+			'resena': "no tan malo"
+		}
 		page = BeautifulSoup(res.data, features="html.parser")
-		self.assertIsNotNone(page.find('form').find('input', type='text'))
-		self.assertIsNotNone(page.find('form').find('input', type='number'))
-		self.assertIsNotNone(page.find('form').find('button', type='submit'))
-		#El cliente le ha dado para atrás para cancelar los cambios realizados
+		self.assertEqual(1, len(page.find('div', class_='row').find_all('div', class_='card')))
+		for card in page.find('div', class_='row').find_all('div', class_='card'):
+			self.assertIn(params['resena'].lower(), card.find(class_='card-text').get_text().lower())
+
 
 
 
