bafaba0333afce94f8b296fc1948826e133c5a88
Jon Marcos Mercadé <104355567+Jonmaa@users.noreply.github.com>
2023-12-22 10:59:53 +0100

    test reseñas empezados

--- tests/test_resenas.py
+++ tests/test_resenas.py
@@ -3,35 +3,29 @@ from bs4 import BeautifulSoup
 
 class TestCatalogo(BaseTestClass):
 	
-	def test_sin_parametros_de_busqueda(self):
-		res = self.client.get('/catalogue')
+	def test_resenasNoLogged(self):
+		res = self.client.get('/resenas')
+		self.assertEqual(302, res.status_code)
+
+
+
+	def test_resenasLogged(self):
+		self.login('jhon@gmail.com','123')
+		res = self.client.get('/resenas')
 		self.assertEqual(200, res.status_code)
-		page = BeautifulSoup(res.data, features="html.parser")
-		self.assertEqual(6,len(page.find('div', class_='row').find_all('div', class_='card')))
+
+	def test_noResenas(self):
+		pass
 
 
-	def test_busquedaFallida(self):
-		params = {
-			'title': "Este libro no existe"
-		}
-		res = self.client.get('/catalogue', query_string = params)
-		self.assertEqual(200, res.status_code)
-		page = BeautifulSoup(res.data, features="html.parser")
-		self.assertEqual(0, len(page.find('div', class_='row').find_all('div', class_='card')))
+	def test_conResenas(self):
+		pass
 
-	def test_busquedaHarryPotter(self):
-		params = {
-			'title': "Harry Potter"
-		}
-		res = self.client.get('/catalogue', query_string = params)
-		page = BeautifulSoup(res.data, features="html.parser")
-		self.assertEqual(6, len(page.find('div', class_='row').find_all('div', class_='card')))
-		for card in page.find('div', class_='row').find_all('div', class_='card'):
-			self.assertIn(params['title'].lower(), card.find(class_='card-title').get_text().lower())
-		self.assertEqual(2, len(page.find('ul', class_='pagination').find_all('li', class_='page-item')))
+	def test_editarResena(self):
+		pass
 
-
-		
+	def test_cancelarEditResena(self):
+		pass
 
 
 
