3e9529960e2bc1a0bae1b47921fbabf0f6bdad93
Etxaniz100 <79299525+Etxaniz100@users.noreply.github.com>
2023-12-22 13:27:05 +0100

    Test foro finalizado

--- tests/test_foro.py
+++ tests/test_foro.py
@@ -77,7 +77,7 @@ class TestForo(BaseTestClass):
 		res = self.client.get('/nuevoTema')
 		self.assertEqual(200, res.status_code)
 
-		#Creamos el tema
+		#Creamos el tema (No podemos operar con formularios con bs4 asi que lo hacemos a mano)
 		cur.execute(f"""INSERT INTO Tema VALUES ('9', 'TemaTest', 'jhon@gmail.com', 'En efecto, esto es un test')""")
 		con.commit()
 
@@ -87,20 +87,9 @@ class TestForo(BaseTestClass):
 
 		page = BeautifulSoup(res.data, features="html.parser")
 
-		print("Hacemos el test : " + str(len(page.find_all('a', class_='card-title'))))
-		self.assertEqual(1, len(page.find_all('a', class_='card-title')))
-
-		self.assertEqual('TemaTest', page.find('div', class_='card-deck').find('div', class_="card").get_text())
+		self.assertEqual('TemaTest', page.find_all('h5')[0].get_text())
 
 
-		#params = {
-		#	'id': "2"
-		#}
-		#res = self.client.get('/tema', query_string=params)
-		#self.assertEqual(200, res.status_code)
-#
-		#page = BeautifulSoup(res.data, features="html.parser")
-		#self.assertEqual('Jhon Doe', page.find('header').find('ul').find_all('li')[-2].get_text())
 
 	def test_crearTemaSinLogear(self):
 		# entrar a crear foro -> redirige a /
@@ -112,8 +101,65 @@ class TestForo(BaseTestClass):
 
 	def test_crearComentarioEstandoLogeado(self):
 		#logear -> entrar a comentar en un tema -> comentar -> comprobar en el tema
-		pass
+
+		cur.execute(f"""DELETE FROM Tema""")
+		con.commit()
+		cur.execute(f"""DELETE FROM Comenta""")
+		con.commit()
+
+		cur.execute(f"""INSERT INTO Tema VALUES ('10', 'TemaTest', 'jhon@gmail.com', 'En efecto, esto es un test')""")
+		con.commit()
+		cur.execute(f"""INSERT INTO Comenta VALUES ('jhon@gmail.com', '10', 'Comentario', '2023-12-09 10:30:24')""")
+		con.commit()
+
+		self.login('jhon@gmail.com', '123')
+
+		params = {
+			'id': "10"
+		}
+		res = self.client.get('/nuevoComentario', query_string = params)
+		self.assertEqual(200, res.status_code)
+
+		#Añadimos el ocmentario a mano
+
+		cur.execute(f"""INSERT INTO Comenta VALUES ('jhon@gmail.com', '10', 'ComentarioTest', '2023-12-10 10:30:24')""")
+		con.commit()
+
+		params = {
+			'id': "10"
+		}
+		res = self.client.get('/tema', query_string = params)
+
+		page = BeautifulSoup(res.data, features="html.parser")
+		self.assertEqual(2, len(page.find_all('div', class_='card-body')))
+
+		#El segundo comentario es que acabamos de añadir por que estan ordenados por fecha
+		self.assertEqual("ComentarioTest", page.find_all('p')[1].get_text())
 
 	def test_crearComentarioSinLogear(self):
 		#entrar a comentar en un tema -> redirige a /
-		pass
+
+		cur.execute(f"""DELETE FROM Tema""")
+		con.commit()
+		cur.execute(f"""DELETE FROM Comenta""")
+		con.commit()
+
+		cur.execute(f"""INSERT INTO Tema VALUES ('10', 'TemaTest', 'jhon@gmail.com', 'En efecto, esto es un test')""")
+		con.commit()
+		cur.execute(f"""INSERT INTO Comenta VALUES ('jhon@gmail.com', '10', 'Comentario', '2023-12-09 10:30:24')""")
+		con.commit()
+
+		#Tratamos de comentar en el tema con id 10
+		params = {
+			'id': "10"
+		}
+		res = self.client.get('/nuevoComentario', query_string=params)
+
+		#Pero como no estamos logeados nos devuelve a home
+		self.assertEqual(302, res.status_code)
+		self.assertEqual('/', res.location)
+
+
+
+
+
