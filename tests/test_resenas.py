3f6688245c3fc8fe6d2681167bcbe6b3f28be342
Jon Marcos Mercadé <104355567+Jonmaa@users.noreply.github.com>
2023-12-22 11:54:56 +0100

    arreglo rápido

--- tests/test_resenas.py
+++ tests/test_resenas.py
@@ -49,6 +49,10 @@ class TestCatalogo(BaseTestClass):
 		con.commit()
 		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
 		self.assertEqual(2, len(res))
+		cur.execute(f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '10', 'ok', '5.0')""")
+		con.commit()
+		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
+		self.assertEqual(3, len(res))
 
 	def test_editarResena(self):
 		self.login('jhon@gmail.com', '123')
@@ -58,10 +62,44 @@ class TestCatalogo(BaseTestClass):
 		con.commit()
 		cur.execute(f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '6', 'muy recomendable para fans de la fantasia', '8.85')""")
 		con.commit()
+		cur.execute(f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '5', 'es un libro vomitivo', '1.1')""")
+		con.commit()
+		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
+		self.assertEqual(2, len(res))
+		res = self.client.get('/editarResena')
+		self.assertEqual(200, res.status_code)
+		page = BeautifulSoup(res.data, features="html.parser")
+		self.assertIsNotNone(page.find('form').find('input', type='text'))
+		self.assertIsNotNone(page.find('form').find('input', type='number'))
+		self.assertIsNotNone(page.find('form').find('button', type='submit'))
+		cur.execute(f"""UPDATE Reseña SET resena = 'no tan malo' WHERE idLibro = '5' AND emailUser = 'jhon@gmail.com'""")
+		con.commit()
+		res = self.client.get('/resenas')
+		self.assertEqual(200, res.status_code)
+
 
 
 	def test_cancelarEditResena(self):
-		pass
+		self.login('jhon@gmail.com', '123')
+		res = self.client.get('/resenas')
+		self.assertEqual(200, res.status_code)
+		cur.execute(f"""DELETE FROM Reseña""")
+		con.commit()
+		cur.execute(
+			f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '6', 'muy recomendable para fans de la fantasia', '8.85')""")
+		con.commit()
+		cur.execute(f"""INSERT INTO Reseña VALUES ('jhon@gmail.com', '5', 'es un libro vomitivo', '1.1')""")
+		con.commit()
+		res = self.db.select(f"SELECT * FROM Reseña WHERE emailUser='jhon@gmail.com'")
+		self.assertEqual(2, len(res))
+		res = self.client.get('/editarResena')
+		self.assertEqual(200, res.status_code)
+		page = BeautifulSoup(res.data, features="html.parser")
+		self.assertIsNotNone(page.find('form').find('input', type='text'))
+		self.assertIsNotNone(page.find('form').find('input', type='number'))
+		self.assertIsNotNone(page.find('form').find('button', type='submit'))
+		#El cliente le ha dado para atrás para cancelar los cambios realizados
+
 
 
 
