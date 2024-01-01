9afc5c49a8772cfc8c941c74e43b64648763f76b
Adrimr7 <adriandecerio@gmail.com>
2024-01-01 14:17:44 +0100

    test_amigos.py Acabado.

--- controller/webServer.py
+++ controller/webServer.py
@@ -1,138 +1,144 @@
 from .LibraryController import LibraryController
 from flask import Flask, render_template, request, make_response, redirect, url_for
-app = Flask(__name__, static_url_path='', static_folder='../view/static', template_folder='../view/')
 
+app = Flask(__name__, static_url_path='', static_folder='../view/static', template_folder='../view/')
 
 library = LibraryController()
 
 
 @app.before_request
 def get_logged_user():
-	# Comprobado que funciona
-	if '/css' not in request.path and '/js' not in request.path:
-		token = request.cookies.get('token')
-		time = request.cookies.get('time')
-		if token and time:
-			request.user = library.get_user_cookies(token, float(time))
-			if request.user:
-				request.user.token = token
+    # Comprobado que funciona
+    if '/css' not in request.path and '/js' not in request.path:
+        token = request.cookies.get('token')
+        time = request.cookies.get('time')
+        if token and time:
+            request.user = library.get_user_cookies(token, float(time))
+            if request.user:
+                request.user.token = token
 
 
 @app.after_request
 def add_cookies(response):
-	if 'user' in dir(request) and request.user and request.user.token:
-		session = request.user.validate_session(request.user.token)
-		response.set_cookie('token', session.hash)
-		response.set_cookie('time', str(session.time))
-	return response
+    if 'user' in dir(request) and request.user and request.user.token:
+        session = request.user.validate_session(request.user.token)
+        response.set_cookie('token', session.hash)
+        response.set_cookie('time', str(session.time))
+    return response
 
 
 @app.route('/')
 def index():
-	if 'user' in dir(request) and request.user and request.user.token:
-		recomendaciones = library.obtenerRecomendaciones(request.user.email)
-		booksRec = []
-		return render_template('index.html', recomendaciones=recomendaciones)
-	else:
-		return render_template('index.html')
+    if 'user' in dir(request) and request.user and request.user.token:
+        recomendaciones = library.obtenerRecomendaciones(request.user.email)
+        booksRec = []
+        return render_template('index.html', recomendaciones=recomendaciones)
+    else:
+        return render_template('index.html')
 
 
 @app.route('/catalogue')
 def catalogue():
-	title = request.values.get("title", "")
-	author = request.values.get("author", "")
-	page = int(request.values.get("page", 1))
-	books, nb_books = library.search_books(title=title, author=author, page=page - 1)
-	total_pages = (nb_books // 6) + 1
-	return render_template('catalogue.html', books=books, title=title, author=author, current_page=page,
-	                       total_pages=total_pages, max=max, min=min)
+    title = request.values.get("title", "")
+    author = request.values.get("author", "")
+    page = int(request.values.get("page", 1))
+    books, nb_books = library.search_books(title=title, author=author, page=page - 1)
+    total_pages = (nb_books // 6) + 1
+    return render_template('catalogue.html', books=books, title=title, author=author, current_page=page,
+                           total_pages=total_pages, max=max, min=min)
+
 
 @app.route('/book')
 def book():
-	id = request.values.get("id", "")
-	book = library.get_book(id)
-	prestado = library.isOnLoan(request.user.email, id)
-	print(prestado)
-	return render_template('book.html', book=book, prestado=prestado)
+    id = request.values.get("id", "")
+    book = library.get_book(id)
+    prestado = library.isOnLoan(request.user.email, id)
+    print(prestado)
+    return render_template('book.html', book=book, prestado=prestado)
+
 
 @app.route('/forum')
 def forum():
-	temas = library.get_temas()
-	return render_template('forum.html', temas=temas)
+    temas = library.get_temas()
+    return render_template('forum.html', temas=temas)
+
 
 @app.route('/tema')
 def tema():
-	id = request.values.get("id", "")
-	#print(id)
-	if id != "":
-		tema = library.get_tema(id)
+    id = request.values.get("id", "")
+    # print(id)
+    if id != "":
+        tema = library.get_tema(id)
 
-		if tema == 0:
-			return redirect('/')
-		comentarios = library.get_comentarios(id)
+        if tema == 0:
+            return redirect('/')
+        comentarios = library.get_comentarios(id)
 
-		return render_template('tema.html', comentarios=comentarios, tema = tema)
-	return redirect('/forum')
+        return render_template('tema.html', comentarios=comentarios, tema=tema)
+    return redirect('/forum')
+
 
 @app.route('/nuevoTema', methods=['GET', 'POST'])
 def nuevoTema():
-	if 'user' in dir(request) and request.user and request.user.token:
-		if request.method == 'POST':
-			titulo = request.form['titulo']
-			descripcion = request.form['descripcion']
-			if titulo == "" :
-				return redirect('/')
+    if 'user' in dir(request) and request.user and request.user.token:
+        if request.method == 'POST':
+            titulo = request.form['titulo']
+            descripcion = request.form['descripcion']
+            if titulo == "":
+                return redirect('/')
 
-			library.nuevoTema(titulo, descripcion, request.user.email)
-			return redirect('/forum')
-		return render_template('nuevoTema.html', )
-	return redirect('/')
+            library.nuevoTema(titulo, descripcion, request.user.email)
+            return redirect('/forum')
+        return render_template('nuevoTema.html', )
+    return redirect('/')
+
 
 @app.route('/nuevoComentario', methods=['GET', 'POST'])
 def nuevoComentario():
-	if 'user' in dir(request) and request.user and request.user.token:
-		if request.method == 'POST':
-			comentario = request.form['comentario']
-			id = request.values.get("id", "")
-			if comentario == "" :
-				return redirect('/')
+    if 'user' in dir(request) and request.user and request.user.token:
+        if request.method == 'POST':
+            comentario = request.form['comentario']
+            id = request.values.get("id", "")
+            if comentario == "":
+                return redirect('/')
 
-			library.nuevoComentario(comentario, request.user.email, id)
-			return redirect('/forum')
-		return render_template('nuevoComentario.html', )
-	return redirect('/')
+            library.nuevoComentario(comentario, request.user.email, id)
+            return redirect('/forum')
+        return render_template('nuevoComentario.html', )
+    return redirect('/')
+
 
 @app.route('/editarResena', methods=['GET', 'POST'])
 def editarResena():
-	if 'user' in dir(request) and request.user and request.user.token:
-		if request.method == 'POST':
-			id = request.values.get("id", "")
-			resena = request.form['resena']
-			if resena == "":
-				resena = library.getResena(id, request.user.email).resena
-			valoracion = request.form['valoracion']
-			try:
-				float(valoracion)
-			except (ValueError, TypeError):
-				valoracion = -1
+    if 'user' in dir(request) and request.user and request.user.token:
+        if request.method == 'POST':
+            id = request.values.get("id", "")
+            resena = request.form['resena']
+            if resena == "":
+                resena = library.getResena(id, request.user.email).resena
+            valoracion = request.form['valoracion']
+            try:
+                float(valoracion)
+            except (ValueError, TypeError):
+                valoracion = -1
 
-			if valoracion == -1:
-				valoracion = library.getResena(id, request.user.email).valoracion
-			if valoracion < 0:
-				valoracion = 0
-			if valoracion > 10:
-				valoracion = 10
-			if resena == "" :
-				return redirect('/resenas')
+            if valoracion == -1:
+                valoracion = library.getResena(id, request.user.email).valoracion
+            if valoracion < 0:
+                valoracion = 0
+            if valoracion > 10:
+                valoracion = 10
+            if resena == "":
+                return redirect('/resenas')
 
-			library.editarResena(resena, request.user.email, id, valoracion)
-			return redirect('/resenas')
-		return render_template('editarResena.html', )
-	return redirect('/')
+            library.editarResena(resena, request.user.email, id, valoracion)
+            return redirect('/resenas')
+        return render_template('editarResena.html', )
+    return redirect('/')
 
 
-#@app.route('/login', methods=['GET', 'POST'])
-#def login():
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
 #	if 'user' in dir(request) and request.user and request.user.token:
 #		return redirect('/')
 #	email = request.values.get("email", "")
@@ -157,132 +163,139 @@ def editarResena():
 
 @app.route('/resena')
 def resena():
-	if 'user' in dir(request) and request.user and request.user.token:
-		id = request.values.get("idLibro", "")
-		resena = library.getResena(id, request.user.email)
-		return render_template('resena.html', resena=resena)
-	return redirect("/")
+    if 'user' in dir(request) and request.user and request.user.token:
+        id = request.values.get("idLibro", "")
+        resena = library.getResena(id, request.user.email)
+        return render_template('resena.html', resena=resena)
+    return redirect("/")
 
 
 @app.route('/resenas')
 def resenas():
-	if 'user' in dir(request) and request.user and request.user.token:
-		email = request.user.email
-		resenas = library.get_resenas(email)
-		return render_template('resenas.html', resenas=resenas)
-	return redirect("/")
+    if 'user' in dir(request) and request.user and request.user.token:
+        email = request.user.email
+        resenas = library.get_resenas(email)
+        return render_template('resenas.html', resenas=resenas)
+    return redirect("/")
 
 
 @app.route('/amigos', methods=['GET', 'POST'])
 def amigos():
-	if 'user' in dir(request) and request.user and request.user.token:
-		if request.method == 'POST':
-			emailUsuario = request.user.email
-			emailSolicitud = request.form['solicitarAmigo']
-			print(emailSolicitud)
-			estaBien = library.enviarSolicitud(emailUsuario, emailSolicitud)
-			if estaBien:
-				print(estaBien)
-				return redirect('/amigos')
-		return render_template('amigos.html')
-	return redirect('/')
+    if 'user' in dir(request) and request.user and request.user.token:
+        if request.method == 'POST':
+            emailUsuario = request.user.email
+            emailSolicitud = request.form['solicitarAmigo']
+            print(emailSolicitud)
+            estaBien = library.enviarSolicitud(emailUsuario, emailSolicitud)
+            if estaBien:
+                print(estaBien)
+                return redirect('/amigos')
+        return render_template('amigos.html')
+    return redirect('/')
+
+
 @app.route('/solicitudes', methods=['GET', 'POST'])
 def solicitudes():
-	if 'user' in dir(request) and request.user and request.user.token:
-		email = request.user.email
-		if request.method == 'POST':
-			emailSolicitud = request.form.get('EmailSolicitud')
-			if 'aceptar' in request.form:
-				library.aceptarSolicitud(email, emailSolicitud)
-			elif 'rechazar' in request.form:
-				library.rechazarSolicitud(email, emailSolicitud)
-		solicitudes = library.getSolicitudes(email)
-		return render_template('solicitudes.html', solicitudes=solicitudes)
-	return redirect('/')
+    if 'user' in dir(request) and request.user and request.user.token:
+        email = request.user.email
+        if request.method == 'POST':
+            emailSolicitud = request.form.get('EmailSolicitud')
+            if 'aceptar' in request.form:
+                library.aceptarSolicitud(email, emailSolicitud)
+            elif 'rechazar' in request.form:
+                library.rechazarSolicitud(email, emailSolicitud)
+        solicitudes = library.getSolicitudes(email)
+        return render_template('solicitudes.html', solicitudes=solicitudes)
+    return redirect('/')
+
+
 @app.route('/verAmigos', methods=['GET', 'POST'])
 def verAmigos():
-	if 'user' in dir(request) and request.user and request.user.token:
-		if request.method == 'POST':
-			return redirect(url_for('verPerfil', emailUsuario = request.form.get('emailUsuario')))
-		else:
-			email = request.user.email
-			amigos = library.get_amigos(email)
-			usuario = request.user.username
-			return render_template('verAmigos.html', amigos=amigos, usuario=usuario)
-	return redirect('/')
+    if 'user' in dir(request) and request.user and request.user.token:
+        if request.method == 'POST':
+            return redirect(url_for('verPerfil', emailUsuario=request.form.get('emailUsuario')))
+        else:
+            email = request.user.email
+            amigos = library.get_amigos(email)
+            usuario = request.user.username
+            return render_template('verAmigos.html', amigos=amigos, usuario=usuario)
+    return redirect('/')
+
+
 @app.route('/verPerfil', methods=['GET', 'POST'])
 def verPerfil():
-	if 'user' in dir(request) and request.user and request.user.token:
-		emailUsuario = request.args.get('emailUsuario')
-		print(emailUsuario)
-		datos = library.obtenerDatosPerfil(emailUsuario)
-		return render_template('verPerfil.html', nomUsuario = datos[0], emailUsuario = datos[1], libros = datos[2])
-	return redirect('/')
+    if 'user' in dir(request) and request.user and request.user.token:
+        emailUsuario = request.args.get('emailUsuario')
+        print(emailUsuario)
+        datos = library.obtenerDatosPerfil(emailUsuario)
+        return render_template('verPerfil.html', nomUsuario=datos[0], emailUsuario=datos[1], libros=datos[2])
+    return redirect('/')
+
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
-	#Comprobamos si el usuario ha iniciado sesion
-	if 'user' in dir(request) and request.user and request.user.token:
-		if request.method == 'POST':
-			#Comprobamos que formulario se ha activado
-			accion1 = request.form.get('accion1') #Registrar usuario
-			accion2 = request.form.get('accion2') #Eliminar usuario
-			accion3 = request.form.get('accion3') #Anadir Libro
-			if accion1 is not None:
-				email = request.form.get('email')
-				nombre = request.form.get('nombre')
-				contrasena = request.form.get('contrasena')
-				admin = request.form.get("admin")
-				completado = library.nuevo_usuario(email, nombre, contrasena, admin)
-			elif accion2 is not None:
-				email = request.form.get('emaile')
+    # Comprobamos si el usuario ha iniciado sesion
+    if 'user' in dir(request) and request.user and request.user.token:
+        if request.method == 'POST':
+            # Comprobamos que formulario se ha activado
+            accion1 = request.form.get('accion1')  # Registrar usuario
+            accion2 = request.form.get('accion2')  # Eliminar usuario
+            accion3 = request.form.get('accion3')  # Anadir Libro
+            if accion1 is not None:
+                email = request.form.get('email')
+                nombre = request.form.get('nombre')
+                contrasena = request.form.get('contrasena')
+                admin = request.form.get("admin")
+                completado = library.nuevo_usuario(email, nombre, contrasena, admin)
+            elif accion2 is not None:
+                email = request.form.get('emaile')
 
-			elif accion3 is not None:
-				titulo = request.form.get('titulo')
-				autor = request.form.get('autor')
-				ncop = request.form.get('ncop')
-				desc = request.form.get('desc')
-				url_portada = request.form.get('portada')
-				completado = library.nuevo_libro(titulo,autor,ncop,desc,url_portada)
-	email = request.user.email
-	admin = library.esAdmin(email)
-	if admin:
-			return render_template('admin.html')
-	else:
-		return redirect("/")
+            elif accion3 is not None:
+                titulo = request.form.get('titulo')
+                autor = request.form.get('autor')
+                ncop = request.form.get('ncop')
+                desc = request.form.get('desc')
+                url_portada = request.form.get('portada')
+                completado = library.nuevo_libro(titulo, autor, ncop, desc, url_portada)
+    email = request.user.email
+    admin = library.esAdmin(email)
+    if admin:
+        return render_template('admin.html')
+    else:
+        return redirect("/")
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if 'user' in dir(request) and request.user and request.user.token:
-		return redirect('/')
-	email = request.values.get("email", "")
-	#print(email)
-	password = request.values.get("password", "")
-	#print(password)
-	user = library.get_user(email, password)
-	#print(user)
-	if user:
-		#print(user.email)
-		session = user.new_session()
-		resp = redirect("/")
-		resp.set_cookie('token', session.hash)
-		resp.set_cookie('time', str(session.time))
-	else:
-		if request.method == 'POST':
-			return redirect('/login')
-		else:
-			resp = render_template('login.html')
-	return resp
+    if 'user' in dir(request) and request.user and request.user.token:
+        return redirect('/')
+    email = request.values.get("email", "")
+    # print(email)
+    password = request.values.get("password", "")
+    # print(password)
+    user = library.get_user(email, password)
+    # print(user)
+    if user:
+        # print(user.email)
+        session = user.new_session()
+        resp = redirect("/")
+        resp.set_cookie('token', session.hash)
+        resp.set_cookie('time', str(session.time))
+    else:
+        if request.method == 'POST':
+            return redirect('/login')
+        else:
+            resp = render_template('login.html')
+    return resp
 
 
 @app.route('/logout')
 def logout():
-	path = request.values.get("path", "/")
-	resp = redirect(path)
-	resp.delete_cookie('token')
-	resp.delete_cookie('time')
-	if 'user' in dir(request) and request.user and request.user.token:
-		request.user.delete_session(request.user.token)
-		request.user = None
-	return resp
+    path = request.values.get("path", "/")
+    resp = redirect(path)
+    resp.delete_cookie('token')
+    resp.delete_cookie('time')
+    if 'user' in dir(request) and request.user and request.user.token:
+        request.user.delete_session(request.user.token)
+        request.user = None
+    return resp
--- tests/test_amigos.py
+++ tests/test_amigos.py
@@ -11,6 +11,7 @@ db_path = os.path.join(script_dir, '..', 'datos.db')
 con = sqlite3.connect(db_path)
 cur = con.cursor()
 
+
 class TestAmigos(BaseTestClass):
     def testVerListaAmigos(self):
         # Acceder a Lista de Amigos
@@ -22,7 +23,7 @@ class TestAmigos(BaseTestClass):
         page = BeautifulSoup(res.data, features="html.parser")
         self.assertEqual(0, len(page.find_all('h5', class_='card-title')))
 
-        # si ahora añadimos un amigo a admin, deberia de tener 1, asi que se comprueba
+        # Si ahora añadimos un amigo a admin, deberia de tener 1, asi que se comprueba
 
         cur.execute(f"""INSERT INTO SonAmigos VALUES ('admin@gmail.com','juan@gmail.com')""")
         con.commit()
@@ -30,9 +31,16 @@ class TestAmigos(BaseTestClass):
         page = BeautifulSoup(res.data, features="html.parser")
         self.assertEqual(1, len(page.find_all('h5', class_='card-title')))
 
-        # añadimos otro amigo, en la posicion contraria de la BD
+        # Añadimos otro amigo, en la posicion contraria de la BD
 
         cur.execute(f"""INSERT INTO SonAmigos VALUES ('jhon@gmail.com','admin@gmail.com')""")
+        con.commit()
+        res = self.client.get('/verAmigos')
+        page = BeautifulSoup(res.data, features="html.parser")
+        self.assertEqual(2, len(page.find_all('h5', class_='card-title')))
+
+        # Si se añade un amigo que no existe, no se deberia de añadir
+        cur.execute(f"""INSERT INTO SonAmigos VALUES ('jhon@gmail.com','usuarioinventado@gmail.com')""")
         con.commit()
         res = self.client.get('/verAmigos')
         page = BeautifulSoup(res.data, features="html.parser")
@@ -46,6 +54,8 @@ class TestAmigos(BaseTestClass):
         cur.execute(f"""INSERT INTO SonAmigos VALUES ('admin@gmail.com','juan@gmail.com')""")
         con.commit()
         cur.execute(f"""INSERT INTO SonAmigos VALUES ('jhon@gmail.com','admin@gmail.com')""")
+        con.commit()
+        cur.execute(f"""INSERT INTO SonAmigos VALUES ('amigojhon@gmail.com','admin@gmail.com')""")
         con.commit()
         cur.execute(f"""DELETE FROM Prestar""")
         con.commit()
@@ -68,16 +78,97 @@ class TestAmigos(BaseTestClass):
         res = self.client.post('/verAmigos', data=dict(emailUsuario='jhon@gmail.com'), follow_redirects=True)
         page = BeautifulSoup(res.data, features="html.parser")
         self.assertEqual(1, len(page.find_all('h5', class_='card-title')))
+        # Aqui comprobamos que efectivamente el usuario es el que dice ser, en este caso Jhon
+        nombre_usuario = page.find_all('h3')[0].text.strip()
+        self.assertEqual('Perfil de Jhon Doe', nombre_usuario)
 
         # Para JUAN tienen que haber TRES libros que haya leido (3)
 
         res = self.client.post('/verAmigos', data=dict(emailUsuario='juan@gmail.com'), follow_redirects=True)
         page = BeautifulSoup(res.data, features="html.parser")
         self.assertEqual(3, len(page.find_all('h5', class_='card-title')))
+        # Aqui comprobamos que efectivamente el usuario es el que dice ser, en este caso Juan
+        nombre_usuario = page.find_all('h3')[0].text.strip()
+        self.assertEqual('Perfil de Juan Ejemplo', nombre_usuario)
 
-    #def testSolicitarAmigo(self):
-        #Hacer una solicitud a un amigo
+        # Para AMIGOJHON no tiene que haber ningún libro (0)
 
-    #def testGestionarSolicitudes(self):
-        #Aceptar o rechazar las solicitudes
+        res = self.client.post('/verAmigos', data=dict(emailUsuario='amigojhon@gmail.com'), follow_redirects=True)
+        page = BeautifulSoup(res.data, features="html.parser")
+        self.assertEqual(0, len(page.find_all('h5', class_='card-title')))
+        # Aqui comprobamos que efectivamente el usuario es el que dice ser, en este caso AmigoJhon
+        nombre_usuario = page.find_all('h3')[0].text.strip()
+        self.assertEqual('Perfil de Amigo Jhon', nombre_usuario)
 
+    def testSolicitarAmigo(self):
+        # Primero se añaden los datos
+        cur.execute(f"""DELETE FROM SonAmigos""")
+        con.commit()
+        cur.execute(f"""INSERT INTO SonAmigos VALUES ('admin@gmail.com','juan@gmail.com')""")
+        con.commit()
+        cur.execute(f"""DELETE FROM Solicita""")
+        con.commit()
+
+        # Ahora mismo JHON y ADMIN no son amigos. Por tanto, si ADMIN solicita a JHON de amigo, veremos que tiene uno.
+        res = self.login('admin@gmail.com', 'admin')
+        res = self.client.post('/amigos', data=dict(solicitarAmigo='jhon@gmail.com'), follow_redirects=True)
+
+        self.client.get('/logout', follow_redirects=True)
+
+        res = self.client.post('/login', data=dict(email='jhon@gmail.com', password='123'), follow_redirects=True)
+        res = self.client.get('/solicitudes')
+
+        page = BeautifulSoup(res.data, features="html.parser")
+        self.assertEqual(1, len(page.find_all('h5', class_='card-title')))
+
+        # Si se borran las solicitudes, no habra ninguna
+        cur.execute(f"""DELETE FROM Solicita""")
+        con.commit()
+
+        res = self.client.get('/solicitudes')
+        page = BeautifulSoup(res.data, features="html.parser")
+        self.assertEqual(0, len(page.find_all('h5', class_='card-title')))
+
+    def testGestionarSolicitudes(self):
+        # Aceptar o rechazar las solicitudes
+        # Para ello primero se añaden los datos correspondientes
+        cur.execute(f"""DELETE FROM SonAmigos""")
+        con.commit()
+        cur.execute(f"""DELETE FROM Solicita""")
+        con.commit()
+        cur.execute(f"""INSERT INTO Solicita VALUES ('admin@gmail.com','jhon@gmail.com')""")
+        con.commit()
+        cur.execute(f"""INSERT INTO Solicita VALUES ('amigojhon@gmail.com','jhon@gmail.com')""")
+        con.commit()
+        # Ahora tiene que haber 2 solicitudes para JHON, por tanto si entramos en solicitudes tiene que haber dos
+        # cajas para poder gestionar la solicitud
+        res = self.login('jhon@gmail.com', '123')
+        res = self.client.get('/solicitudes')
+        page = BeautifulSoup(res.data, features="html.parser")
+        self.assertEqual(2, len(page.find_all('h5', class_='card-title')))
+        # Lo siguiente es comprobar que al rechazar una solicitud, esos dos usuarios no son amigos, utilizando la misma
+        # lógica que en el primer test
+
+        res = self.client.post('/solicitudes', data=dict(EmailSolicitud='admin@gmail.com', rechazar=''), follow_redirects=True)
+        res = self.client.get('/verAmigos')
+        page = BeautifulSoup(res.data, features="html.parser")
+        self.assertEqual(0, len(page.find_all('h5', class_='card-title')))
+
+        # Ademas de comprobar que no se ha añadido, hay que ver que en solicita se haya rechazado, esto es,
+        # ver si no está en solicitudes, como en el paso anterior.
+        res = self.client.get('/solicitudes')
+        page = BeautifulSoup(res.data, features="html.parser")
+        self.assertEqual(1, len(page.find_all('h5', class_='card-title')))
+
+        # Ahora comprobamos aceptando una solicitud, y despues se comprueba tambien que se haya quitado de las
+        # solicitudes
+        res = self.client.post('/solicitudes', data=dict(EmailSolicitud='amigojhon@gmail.com', aceptar=''), follow_redirects=True)
+        res = self.client.get('/verAmigos')
+        page = BeautifulSoup(res.data, features="html.parser")
+        self.assertEqual(1, len(page.find_all('h5', class_='card-title')))
+
+        # Ademas de comprobar que no se ha añadido, hay que ver que en solicita se haya aceptado, esto es,
+        # ver si no está en solicitudes, como en el paso anterior.
+        res = self.client.get('/solicitudes')
+        page = BeautifulSoup(res.data, features="html.parser")
+        self.assertEqual(0, len(page.find_all('h5', class_='card-title')))
